71a3f05626cb68eb62eb326fa64a86faa62bf601
livorish <hoksini@gmail.com>
2023-11-26 18:13:56 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?w=740&t=st=1701000764~exp=1701001364~hmac=a9afc7a526c507b6d410f90b18c611660341c7514c552dff41f54e35764751fa"/>
+  </header>;
+}
+
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+      <img src="https://img.freepik.com/free-photo/blue-foaming-waves-in-north-beach-in-nazare-portugal_1268-15934.jpg?w=900&t=st=1701003035~exp=1701003635~hmac=f37e9b7447b676f3e6f3b1f604fe7b6418f7bcfce8894131b586ec7db486f8cc" />
+    </div>
+    <div>
+      ava + descr
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+    <div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+    </div>
+  </div>
+}
+
+
+export default Profile;
